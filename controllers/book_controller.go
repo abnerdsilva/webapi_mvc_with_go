41f2413b8ed8bf10e10dd8ebb0c1bdc158e0386f
Abner Silva <abner.computadores@outlook.com>
2021-05-18 10:51:18 -0300

    add endpoints de books (get, post, update e delete)

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -1,9 +1,127 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/abnerdsilva/webapi_mvc_with_go/database"
+	"github.com/abnerdsilva/webapi_mvc_with_go/models"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+func ShowBook(c *gin.Context) {
+	id := c.Param("id")
+	newid, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+
+		return
+	}
+
+	db := database.GetDatabase()
+
+	var book models.Book
+	err = db.First(&book, newid).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot find book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func CreateBook(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot bind JSON: " + err.Error(),
+		})
+
+		return
+	}
+
+	err = db.Create(&book).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot create book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
 
 func ShowBooks(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "OK",
-	})
+	db := database.GetDatabase()
+
+	var books []models.Book
+	err := db.Find(&books).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot list books: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, books)
+}
+
+func UpdateBook(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot bind JSON: " + err.Error(),
+		})
+
+		return
+	}
+
+	err = db.Save(&book).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot create book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func DeleteBook(c *gin.Context) {
+	id := c.Param("id")
+	newid, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+
+		return
+	}
+
+	db := database.GetDatabase()
+
+	err = db.Delete(&models.Book{}, newid).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot delete book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.Status(204)
 }
